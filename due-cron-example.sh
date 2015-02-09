1b4613bdfa0c725409e5e2bcd8fa581823c7b847
kmacleod <kmacleod@nakinasystems.com>
2015-02-09 10:59:41 -0500

    update cron example with markdown email

--- due-cron-example.sh
+++ due-cron-example.sh
@@ -1,8 +1,30 @@
 #!/bin/bash
 
-MAILTO=<your email address>
+MAILTO="<your email address>"
+EMAIL_HEADER="todo.txt report"
+#MARKDOWN="/usr/bin/markdown"
+MARKDOWN=""
 
-report=$(/usr/bin/todo.sh due)
+outfile=/tmp/due-cron.out
+
+send_html_mail()
+{
+  (
+  echo "To: $MAILTO"
+  echo "Subject: $EMAIL_HEADER"
+  echo "Content-Type: text/html"
+  echo ""
+  $MARKDOWN "$outfile"
+  echo ""
+  ) | /usr/sbin/sendmail -t
+}
+
+/usr/bin/todo.sh due > $outfile 2>&1
+report=$(cat $outfile)
 if [ ! -z "$report" ]; then
-  echo $report | mail -v -s "todo.txt due $(date)" "$MAILTO"
+  if [ ! -z "$MARKDOWN" ]; then
+    send_html_mail
+  else
+    cat $outfile | mail -s "$EMAIL_HEADER" "$MAILTO"
+  fi
 fi
