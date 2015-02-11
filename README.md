b7bafc5435803486ab0755c660162a0d7a9794db
kmacleod <kmacleod@nakinasystems.com>
2015-02-11 17:45:57 -0500

    Changes before publishing
    - Support python 3
    - Expand cron example
    - Update readme

--- README.md
+++ README.md
@@ -8,5 +8,19 @@ The report lists:
 * overdue items
 * due items (items due today)
 
-The report format is simple markdown text, suitable for sending via email or piping through a markdown script to generate HTML.
+The report is formatted as simple markdown text, suitable for sending via email or piping through a markdown script to generate HTML.
+
+See the `due-cron-example.sh` script for an example script to be invoked via cron.
+
+
+### Dependencies
+
+You need python installed. Anything at or above 2.7 should work. It also works in 3.x.
+
+
+### Usage
+
+Copy the `due` script into your `$HOME/.todo.actions.d` directory (or wherever you have your addons configured). Ensure it's executable, then you can just use it as a standard todo.sh addon:
+
+    todo.sh due
 
--- due-cron-example.sh
+++ due-cron-example.sh
@@ -1,14 +1,34 @@
 #!/bin/bash
 
+# This is a simple script to be invoked via cron. It runs `todo.sh due` and will send an email only if there are reported items.
+# Usage:
+#
+# 1. Configure the MAILTO variable below
+#
+# 2. Setup cron. A sample crontabl entry:
+# Send email at 6am:
+# 0 6 * * *   /path/to/due-cron.sh > /tmp/cron-due.log 2>&1
+
+
+# -- Configuration
+
 # your email address
 MAILTO="<your email addres>"
+
+# path to todo.sh
+TODO_SH=${TODO_SH:="/usr/bin/todo.sh"}
+
+# path to markdown - leave blank to send text email
+#MARKDOWN=""
+MARKDOWN=${MARKDOWN:="/usr/bin/markdown"}
 
 # the email header
 EMAIL_HEADER="todo.txt report"
 
-# path to markdown - leave blank to send text email
-#MARKDOWN=""
-MARKDOWN="/usr/bin/markdown"
+# path to sendmail
+SENDMAIL=${SENDMAIL:="/usr/bin/sendmail"}
+
+# -- End of configuration
 
 
 outfile=/tmp/due-cron.out
@@ -23,10 +43,10 @@ send_html_mail()
   echo ""
   $MARKDOWN "$outfile"
   echo ""
-  ) | /usr/sbin/sendmail -t
+  ) | $SENDMAIL -t
 }
 
-/usr/bin/todo.sh due > $outfile 2>&1
+$TODO_SH due > $outfile 2>&1
 report=$(cat $outfile)
 if [ ! -z "$report" ]; then
   if [ ! -z "$MARKDOWN" ]; then
