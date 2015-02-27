4d6ec9a6f5476d4d45fcdd1a8a71471d07d031ea
kmacleod <kmacleod@nakinasystems.com>
2015-02-27 09:40:25 -0500

    Move header into help

--- todo-add-due.sh
+++ todo-add-due.sh
@@ -1,23 +1,29 @@
 #!/bin/bash
 
-# This script adds a todo item which is due in a specified number of days.
-# The intention is to invoke this from cron, to schedule periodic todo items.
-# 
-# NOTE: it's probably a good idea to make sure none of these run at the same time.
-#
-# An example set of crontab entries:
-#
-## todo.sh recurring items: Note: make sure none of these run at the same time
-#1 4 * * 2  /somepath/todo-due/todo-add-due.sh -d 0 "(C) @home Take out the trash" > /tmp/todo.sh.log 2>&1
-#0 4 12 * *  /somepath/todo-due/todo-add-due.sh -d 5 -t 2 "(B) @computer Schedule VISA payment" > /tmp/todo.sh.log 2>&1
-#0 4 1 * *  /somepath/todo-due/todo-add-due.sh -d 0 "@work Archive monthly email" > /tmp/todo.sh.log 2>&1
-#5 4 1 1,3,6,9 *  /somepath/todo-due/todo-add-due.sh -d 0 "@work Cleanup downloaded packages" > /tmp/todo.sh.log 2>&1
-
 help()
 {
-  printf "usage: $(basename $0) -d <days> ?-t? <days> <todo_item>\n"
-  printf "\n   -d <days>: number of days until due\n"
-  printf "\n   -t <days>: number of days until threshold (optional)\n"
+cat <<HELP
+usage: $(basename $0) -d <days> ?-t? <days> <todo_item>
+
+options:
+   -d <days>: number of days until due
+   -t <days>: number of days until threshold (optional)
+
+This script adds a todo item to be due in a specified number of days.
+
+You can invoke this script from cron to schedule periodic todo items.
+ 
+
+An example set of crontab entries
+NOTE: it's probably a good idea to make sure none of these run at the same time:
+
+# todo.sh recurring items: Note: make sure none of these run at the same time
+1 4 * * 2  /somepath/todo-due/todo-add-due.sh -d 0 "(C) @home Take out the trash" > /tmp/todo.sh.log 2>&1
+0 4 12 * *  /somepath/todo-due/todo-add-due.sh -d 5 -t 2 "(B) @computer Schedule VISA payment" > /tmp/todo.sh.log 2>&1
+0 4 1 * *  /somepath/todo-due/todo-add-due.sh -d 0 "@work Archive monthly email" > /tmp/todo.sh.log 2>&1
+5 4 1 1,3,6,9 *  /somepath/todo-due/todo-add-due.sh -d 0 "@work Cleanup downloaded packages" > /tmp/todo.sh.log 2>&1
+
+HELP
   exit 1
 }
 
